9e0e1664e9024080cea455c61868889c424219b7
Apollos Burcham <Apollos@MacBook-Pro.local>
2018-08-01 16:45:22 -0500

    Complete Layout for Projects Section

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
-  AOS.init({
-  duration: 1200,
-});
+
+  $('.carousel').slick({
+  arrows:false,
+  centerMode:true,
+  centerMode: true,
+  slidesToScroll: 1,
+  autoplay:true,
+  autoplaySpeed:2000,
+  fade: true,
+  cssEase:'linear',
+  draggable:false,
+  pauseOnFocus:false,
+  pauseOnHover:false
+  });
+
   $('.navbar-toggler').on('click', function () {
     $('.animated-icon4').toggleClass('open');
 });
